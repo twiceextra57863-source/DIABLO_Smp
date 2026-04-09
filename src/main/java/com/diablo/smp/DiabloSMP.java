e28fe3fe36ca07ddd4d333360b277cba21e24592
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-09 15:11:08 +0530

    Update DiabloSMP.java

--- src/main/java/com/diablo/smp/DiabloSMP.java
+++ src/main/java/com/diablo/smp/DiabloSMP.java
@@ -1,56 +1,26 @@
 package com.diablo.smp;
 
-import com.diablo.smp.commands.DiabloCommand;
-import com.diablo.smp.commands.TrustCommand;
-import com.diablo.smp.listeners.AbilityListener;
-import com.diablo.smp.listeners.SecurityListener;
-import com.diablo.smp.manager.AbilityManager;
-import com.diablo.smp.manager.TrustManager;
+import com.diablo.smp.commands.*;
+import com.diablo.smp.listeners.*;
+import com.diablo.smp.manager.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class DiabloSMP extends JavaPlugin {
-
-    private static DiabloSMP instance;
+public class DiabloSMP extends JavaPlugin {
     private AbilityManager abilityManager;
     private TrustManager trustManager;
 
     @Override
     public void onEnable() {
-        instance = this;
-        saveDefaultConfig();
+        this.abilityManager = new AbilityManager();
+        this.trustManager = new TrustManager();
 
-        // Initialize Managers
-        this.abilityManager = new AbilityManager(this);
-        this.trustManager = new TrustManager(this);
+        getServer().getPluginManager().registerEvents(new SecurityListener(this), this);
+        getServer().getPluginManager().registerEvents(new AbilityListener(this), this);
 
-        // Register Commands
         getCommand("diablo").setExecutor(new DiabloCommand(this));
         getCommand("trust").setExecutor(new TrustCommand(this));
-
-        // Register Listeners
-        getServer().getPluginManager().registerEvents(new AbilityListener(this), this);
-        getServer().getPluginManager().registerEvents(new SecurityListener(this), this);
-
-        getLogger().info("DiabloSMP has been enabled!");
     }
 
-    @Override
-    public void onDisable() {
-        if (trustManager != null) {
-            trustManager.saveTrusts();
-        }
-        getLogger().info("DiabloSMP has been disabled!");
-    }
-
-    public static DiabloSMP getInstance() {
-        return instance;
-    }
-
-    public AbilityManager getAbilityManager() {
-        return abilityManager;
-    }
-
-    public TrustManager getTrustManager() {
-        return trustManager;
-    }
+    public AbilityManager getAbilityManager() { return abilityManager; }
+    public TrustManager getTrustManager() { return trustManager; }
 }
