3fc7cf0fa7173695bb8a9b73e7375d262261c12f
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-09 15:15:56 +0530

    Update DiabloSMP.java

--- src/main/java/com/diablo/smp/DiabloSMP.java
+++ src/main/java/com/diablo/smp/DiabloSMP.java
@@ -1,26 +1,33 @@
 package com.diablo.smp;
 
-import com.diablo.smp.commands.*;
-import com.diablo.smp.listeners.*;
-import com.diablo.smp.manager.*;
+import com.diablo.smp.commands.AdminCommand;
+import com.diablo.smp.commands.TrustCommand;
+import com.diablo.smp.listeners.AbilityHandler;
+import com.diablo.smp.listeners.SecurityHandler;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.util.*;
 
 public class DiabloSMP extends JavaPlugin {
-    private AbilityManager abilityManager;
-    private TrustManager trustManager;
+    
+    // Player UUID -> Current Stage (1, 2, 3)
+    public final Map<UUID, Integer> playerStage = new HashMap<>();
+    // Player UUID -> AbilityName_Stage -> Cooldown End Time
+    public final Map<UUID, Map<String, Long>> cooldowns = new HashMap<>();
+    // Trust Contract: FromPlayer -> ToPlayer (Ends in 5 mins)
+    public final Map<UUID, UUID> trustContract = new HashMap<>();
+    public final Map<UUID, Long> trustExpiry = new HashMap<>();
+    
+    public final Set<UUID> isStatue = new HashSet<>();
 
     @Override
     public void onEnable() {
-        this.abilityManager = new AbilityManager();
-        this.trustManager = new TrustManager();
-
-        getServer().getPluginManager().registerEvents(new SecurityListener(this), this);
-        getServer().getPluginManager().registerEvents(new AbilityListener(this), this);
-
-        getCommand("diablo").setExecutor(new DiabloCommand(this));
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new AbilityHandler(this), this);
+        getServer().getPluginManager().registerEvents(new SecurityHandler(this), this);
+        
+        getCommand("diablo").setExecutor(new AdminCommand(this));
         getCommand("trust").setExecutor(new TrustCommand(this));
+        
+        getLogger().info("§4[DiabloSMP] §fPlugin Fully Loaded! 🗡️");
     }
-
-    public AbilityManager getAbilityManager() { return abilityManager; }
-    public TrustManager getTrustManager() { return trustManager; }
 }
