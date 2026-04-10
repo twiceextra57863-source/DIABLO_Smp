7313eaa3d5ea0d1a36499ce76bf0d8b26dc3d089
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-10 11:28:23 +0530

    Update AbilityCommand.java

--- src/main/java/com/diablo/commands/AbilityCommand.java
+++ src/main/java/com/diablo/commands/AbilityCommand.java
@@ -26,98 +26,20 @@ public class AbilityCommand implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length == 0) {
-            sendHelp(sender);
-            return true;
-        }
-        
-        switch (args[0].toLowerCase()) {
-            case "give":
-                if (!sender.hasPermission("diablosmp.admin")) {
-                    sender.sendMessage(ChatColor.RED + "You don't have permission!");
-                    return true;
-                }
-                if (args.length < 3) {
-                    sender.sendMessage(ChatColor.RED + "Usage: /ability give <player> <ability>");
-                    return true;
-                }
-                giveAbility(sender, args[1], args[2]);
-                break;
-                
-            case "remove":
-                if (!sender.hasPermission("diablosmp.admin")) {
-                    sender.sendMessage(ChatColor.RED + "You don't have permission!");
-                    return true;
-                }
-                if (args.length < 3) {
-                    sender.sendMessage(ChatColor.RED + "Usage: /ability remove <player> <ability>");
-                    return true;
-                }
-                removeAbility(sender, args[1], args[2]);
-                break;
-                
-            case "list":
-                listAbilities(sender, args.length > 1 ? args[1] : null);
-                break;
-                
-            case "reload":
-                if (!sender.hasPermission("diablosmp.admin")) {
-                    sender.sendMessage(ChatColor.RED + "You don't have permission!");
-                    return true;
-                }
-                plugin.reloadConfig();
-                sender.sendMessage(ChatColor.GREEN + "Diablo SMP Plugin reloaded!");
-                break;
-                
-            case "info":
-                showInfo(sender);
-                break;
-                
-            default:
-                sendHelp(sender);
-                break;
-        }
-        
-        return true;
-package com.diablosmp.commands;
-
-import com.diablosmp.DiabloSmpPlugin;
-import com.diablosmp.abilities.AbilityType;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
-import org.bukkit.entity.Player;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-public class AbilityCommand implements CommandExecutor, TabCompleter {
-    
-    private final DiabloSmpPlugin plugin;
-    
-    public AbilityCommand(DiabloSmpPlugin plugin) {
-        this.plugin = plugin;
-    }
-    
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length == 0) {
             sendGrimoire(sender);
             return true;
         }
         
-        switch (args[0].toLowerCase()) {
+        String subCommand = args[0].toLowerCase();
+        
+        switch (subCommand) {
             case "bestow":
                 if (!sender.hasPermission("abyssal.archon")) {
-                    sender.sendMessage(ChatColor.DARK_RED + "⬡ " + ChatColor.RED + "You lack the Archon's authority! ⬡");
+                    sender.sendMessage(ChatColor.DARK_RED + "[Abyss] " + ChatColor.RED + "You lack the Archon's authority!");
                     return true;
                 }
                 if (args.length < 3) {
-                    sender.sendMessage(ChatColor.DARK_PURPLE + "⬡ " + ChatColor.LIGHT_PURPLE + "Usage: /abyssal bestow <player> <power> ⬡");
+                    sender.sendMessage(ChatColor.DARK_PURPLE + "[Abyss] " + ChatColor.LIGHT_PURPLE + "Usage: /abyssal bestow <player> <power>");
                     return true;
                 }
                 bestowPower(sender, args[1], args[2]);
@@ -125,11 +47,11 @@ public class AbilityCommand implements CommandExecutor, TabCompleter {
                 
             case "cleanse":
                 if (!sender.hasPermission("abyssal.archon")) {
-                    sender.sendMessage(ChatColor.DARK_RED + "⬡ " + ChatColor.RED + "You lack the Archon's authority! ⬡");
+                    sender.sendMessage(ChatColor.DARK_RED + "[Abyss] " + ChatColor.RED + "You lack the Archon's authority!");
                     return true;
                 }
                 if (args.length < 3) {
-                    sender.sendMessage(ChatColor.DARK_PURPLE + "⬡ " + ChatColor.LIGHT_PURPLE + "Usage: /abyssal cleanse <player> <power> ⬡");
+                    sender.sendMessage(ChatColor.DARK_PURPLE + "[Abyss] " + ChatColor.LIGHT_PURPLE + "Usage: /abyssal cleanse <player> <power>");
                     return true;
                 }
                 cleansePower(sender, args[1], args[2]);
@@ -145,11 +67,11 @@ public class AbilityCommand implements CommandExecutor, TabCompleter {
                 
             case "awakening":
                 if (!sender.hasPermission("abyssal.archon")) {
-                    sender.sendMessage(ChatColor.DARK_RED + "⬡ " + ChatColor.RED + "You lack the Archon's authority! ⬡");
+                    sender.sendMessage(ChatColor.DARK_RED + "[Abyss] " + ChatColor.RED + "You lack the Archon's authority!");
                     return true;
                 }
                 plugin.reloadConfig();
-                sender.sendMessage(ChatColor.DARK_PURPLE + "⬡ " + ChatColor.LIGHT_PURPLE + "The Abyss has been awakened anew! ⬡");
+                sender.sendMessage(ChatColor.DARK_PURPLE + "[Abyss] " + ChatColor.LIGHT_PURPLE + "The Abyss has been awakened anew!");
                 break;
                 
             default:
@@ -163,85 +85,79 @@ public class AbilityCommand implements CommandExecutor, TabCompleter {
     private void bestowPower(CommandSender sender, String playerName, String powerName) {
         Player target = Bukkit.getPlayer(playerName);
         if (target == null) {
-            sender.sendMessage(ChatColor.DARK_RED + "⬡ " + ChatColor.RED + "That soul does not exist in this realm! ⬡");
+            sender.sendMessage(ChatColor.DARK_RED + "[Abyss] " + ChatColor.RED + "That soul does not exist in this realm!");
             return;
         }
         
         AbilityType type = getPowerByName(powerName);
         if (type == null) {
-            sender.sendMessage(ChatColor.DARK_RED + "⬡ " + ChatColor.RED + "Unknown Abyssal Power! Available powers: " + getPowerList() + " ⬡");
+            sender.sendMessage(ChatColor.DARK_RED + "[Abyss] " + ChatColor.RED + "Unknown Abyssal Power! Available: " + getPowerList());
             return;
         }
         
         plugin.getAbilityManager().giveAbility(target, type);
-        sender.sendMessage(type.getChatColor() + "⬡ " + ChatColor.GOLD + "Bestowed " + 
-            type.getDisplayName() + ChatColor.GOLD + " upon " + target.getName() + " ⬡");
+        sender.sendMessage(type.getChatColor() + "[Abyss] " + ChatColor.GOLD + "Bestowed " + 
+            type.getDisplayName() + ChatColor.GOLD + " upon " + target.getName());
     }
     
     private void cleansePower(CommandSender sender, String playerName, String powerName) {
         Player target = Bukkit.getPlayer(playerName);
         if (target == null) {
-            sender.sendMessage(ChatColor.DARK_RED + "⬡ " + ChatColor.RED + "That soul does not exist in this realm! ⬡");
+            sender.sendMessage(ChatColor.DARK_RED + "[Abyss] " + ChatColor.RED + "That soul does not exist in this realm!");
             return;
         }
         
         AbilityType type = getPowerByName(powerName);
         if (type == null) {
-            sender.sendMessage(ChatColor.DARK_RED + "⬡ " + ChatColor.RED + "Unknown Abyssal Power! ⬡");
+            sender.sendMessage(ChatColor.DARK_RED + "[Abyss] " + ChatColor.RED + "Unknown Abyssal Power!");
             return;
         }
         
         plugin.getAbilityManager().removeAbility(target, type);
-        sender.sendMessage(type.getChatColor() + "⬡ " + ChatColor.RED + "Cleansed " + 
-            type.getDisplayName() + ChatColor.RED + " from " + target.getName() + "'s soul ⬡");
+        sender.sendMessage(type.getChatColor() + "[Abyss] " + ChatColor.RED + "Cleansed " + 
+            type.getDisplayName() + ChatColor.RED + " from " + target.getName() + "'s soul");
     }
     
     private void showCompendium(CommandSender sender) {
         sender.sendMessage("");
-        sender.sendMessage(ChatColor.DARK_PURPLE + "⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡");
-        sender.sendMessage(ChatColor.LIGHT_PURPLE + "       ᑕOᗰᑭEᑎᗪIᑌᗰ Oᖴ ᗩᗷYᔕᔕᗩᒪ ᑭOᗯEᖇᔕ");
-        sender.sendMessage(ChatColor.DARK_PURPLE + "⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡");
+        sender.sendMessage(ChatColor.DARK_PURPLE + "===== " + ChatColor.LIGHT_PURPLE + "COMPENDIUM OF ABYSSAL POWERS" + ChatColor.DARK_PURPLE + " =====");
         sender.sendMessage("");
         
         for (AbilityType type : AbilityType.values()) {
-            sender.sendMessage(type.getChatColor() + "  ⬡ " + ChatColor.GOLD + type.getSimpleName() + 
+            sender.sendMessage(type.getChatColor() + "  * " + ChatColor.GOLD + type.getSimpleName() + 
                 ChatColor.GRAY + " - " + type.getDisplayName());
         }
         
         sender.sendMessage("");
         sender.sendMessage(ChatColor.DARK_GRAY + "Use /abyssal compendium <player> to view awakened powers");
-        sender.sendMessage(ChatColor.DARK_PURPLE + "⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡");
+        sender.sendMessage(ChatColor.DARK_PURPLE + "================================================");
     }
     
     private void showPlayerCompendium(CommandSender sender, String playerName) {
         Player target = Bukkit.getPlayer(playerName);
         if (target == null) {
-            sender.sendMessage(ChatColor.DARK_RED + "⬡ " + ChatColor.RED + "That soul does not exist in this realm! ⬡");
+            sender.sendMessage(ChatColor.DARK_RED + "[Abyss] " + ChatColor.RED + "That soul does not exist in this realm!");
             return;
         }
         
         List<AbilityType> abilities = plugin.getAbilityManager().getPlayerAbilities(target);
         
         sender.sendMessage("");
-        sender.sendMessage(ChatColor.DARK_PURPLE + "⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡");
-        sender.sendMessage(ChatColor.LIGHT_PURPLE + "   " + target.getName() + "'ᔕ ᗩᗯᗩKEᑎEᗪ ᑭOᗯEᖇᔕ");
-        sender.sendMessage(ChatColor.DARK_PURPLE + "⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡");
+        sender.sendMessage(ChatColor.DARK_PURPLE + "===== " + ChatColor.LIGHT_PURPLE + target.getName() + "'s AWAKENED POWERS" + ChatColor.DARK_PURPLE + " =====");
         
         if (abilities.isEmpty()) {
             sender.sendMessage(ChatColor.GRAY + "   This soul has not awakened any powers...");
         } else {
             for (AbilityType type : abilities) {
-                sender.sendMessage(type.getChatColor() + "   ⬡ " + type.getDisplayName());
+                sender.sendMessage(type.getChatColor() + "   * " + type.getDisplayName());
             }
         }
-        sender.sendMessage(ChatColor.DARK_PURPLE + "⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡");
+        sender.sendMessage(ChatColor.DARK_PURPLE + "================================================");
     }
     
     private void sendGrimoire(CommandSender sender) {
         sender.sendMessage("");
-        sender.sendMessage(ChatColor.DARK_PURPLE + "⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡");
-        sender.sendMessage(ChatColor.LIGHT_PURPLE + "         GᖇIᗰOIᖇE Oᖴ TᕼE ᗩᗷYᔕᔕ");
-        sender.sendMessage(ChatColor.DARK_PURPLE + "⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡");
+        sender.sendMessage(ChatColor.DARK_PURPLE + "===== " + ChatColor.LIGHT_PURPLE + "GRIMOIRE OF THE ABYSS" + ChatColor.DARK_PURPLE + " =====");
         sender.sendMessage("");
         sender.sendMessage(ChatColor.GOLD + "/abyssal bestow <player> <power> " + ChatColor.GRAY + "- Grant Abyssal Power");
         sender.sendMessage(ChatColor.GOLD + "/abyssal cleanse <player> <power> " + ChatColor.GRAY + "- Remove Abyssal Power");
@@ -249,14 +165,15 @@ public class AbilityCommand implements CommandExecutor, TabCompleter {
         sender.sendMessage(ChatColor.GOLD + "/abyssal awakening " + ChatColor.GRAY + "- Reload the Abyss");
         sender.sendMessage(ChatColor.GOLD + "/soulbind <player> " + ChatColor.GRAY + "- Bind souls for 5 minutes");
         sender.sendMessage("");
-        sender.sendMessage(ChatColor.DARK_PURPLE + "⬡ " + ChatColor.LIGHT_PURPLE + "Double Crouch to cycle through power stages! " + ChatColor.DARK_PURPLE + "⬡");
-        sender.sendMessage(ChatColor.DARK_PURPLE + "⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡⬡");
+        sender.sendMessage(ChatColor.DARK_PURPLE + "* " + ChatColor.LIGHT_PURPLE + "Double Crouch to cycle through power stages! " + ChatColor.DARK_PURPLE + "*");
+        sender.sendMessage(ChatColor.DARK_PURPLE + "=========================================");
     }
     
     private AbilityType getPowerByName(String name) {
         for (AbilityType type : AbilityType.values()) {
             if (type.getSimpleName().equalsIgnoreCase(name) || 
-                type.name().equalsIgnoreCase(name)) {
+                type.name().equalsIgnoreCase(name) ||
+                type.getDisplayName().toLowerCase().contains(name.toLowerCase())) {
                 return type;
             }
         }
@@ -274,23 +191,43 @@ public class AbilityCommand implements CommandExecutor, TabCompleter {
         List<String> completions = new ArrayList<>();
         
         if (args.length == 1) {
-            completions.addAll(Arrays.asList("bestow", "cleanse", "compendium", "awakening"));
+            String partial = args[0].toLowerCase();
+            List<String> subCommands = Arrays.asList("bestow", "cleanse", "compendium", "awakening");
+            for (String sub : subCommands) {
+                if (sub.startsWith(partial)) {
+                    completions.add(sub);
+                }
+            }
         } else if (args.length == 2) {
-            if (args[0].equalsIgnoreCase("bestow") || args[0].equalsIgnoreCase("cleanse")) {
-                Bukkit.getOnlinePlayers().forEach(p -> completions.add(p.getName()));
-            } else if (args[0].equalsIgnoreCase("compendium")) {
-                Bukkit.getOnlinePlayers().forEach(p -> completions.add(p.getName()));
+            String subCommand = args[0].toLowerCase();
+            String partial = args[1].toLowerCase();
+            
+            if (subCommand.equals("bestow") || subCommand.equals("cleanse")) {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    if (player.getName().toLowerCase().startsWith(partial)) {
+                        completions.add(player.getName());
+                    }
+                }
+            } else if (subCommand.equals("compendium")) {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    if (player.getName().toLowerCase().startsWith(partial)) {
+                        completions.add(player.getName());
+                    }
+                }
             }
         } else if (args.length == 3) {
-            if (args[0].equalsIgnoreCase("bestow") || args[0].equalsIgnoreCase("cleanse")) {
+            String subCommand = args[0].toLowerCase();
+            String partial = args[2].toLowerCase();
+            
+            if (subCommand.equals("bestow") || subCommand.equals("cleanse")) {
                 for (AbilityType type : AbilityType.values()) {
-                    completions.add(type.getSimpleName());
+                    if (type.getSimpleName().toLowerCase().startsWith(partial)) {
+                        completions.add(type.getSimpleName());
+                    }
                 }
             }
         }
         
-        return completions.stream()
-            .filter(s -> s.toLowerCase().startsWith(args[args.length - 1].toLowerCase()))
-            .collect(Collectors.toList());
+        return completions;
     }
 }
