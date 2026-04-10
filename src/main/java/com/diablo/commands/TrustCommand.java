6a62e5b677656c6445625f0eafad0263fd2f71a0
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-10 12:12:31 +0530

    Update TrustCommand.java

--- src/main/java/com/diablo/commands/TrustCommand.java
+++ src/main/java/com/diablo/commands/TrustCommand.java
@@ -3,7 +3,6 @@ package com.diablosmp.commands;
 import com.diablosmp.DiabloSmpPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Particle;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -25,49 +24,32 @@ public class TrustCommand implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.DARK_RED + "[Abyss] " + ChatColor.RED + "Only living souls can perform the Soul Binding!");
+            sender.sendMessage(ChatColor.RED + "Only players can use this command!");
             return true;
         }
         
         Player player = (Player) sender;
         
         if (args.length < 1) {
-            sendBindingHelp(player);
+            player.sendMessage(ChatColor.RED + "Usage: /trust <player>");
+            player.sendMessage(ChatColor.GRAY + "Trust another player to receive ability tomes for 5 minutes");
             return true;
         }
         
         Player target = Bukkit.getPlayer(args[0]);
         if (target == null) {
-            player.sendMessage(ChatColor.DARK_RED + "[Abyss] " + ChatColor.RED + "That soul does not exist in this realm!");
+            player.sendMessage(ChatColor.RED + "Player not found!");
             return true;
         }
         
         if (target.equals(player)) {
-            player.sendMessage(ChatColor.DARK_RED + "[Abyss] " + ChatColor.RED + "You cannot bind your soul to yourself!");
+            player.sendMessage(ChatColor.RED + "You cannot trust yourself!");
             return true;
         }
         
         plugin.getTrustManager().createTrust(player, target);
         
-        // Visual effect for soul binding
-        player.getWorld().spawnParticle(Particle.SOUL_FIRE_FLAME, 
-            player.getLocation().add(0, 1, 0), 20, 0.5, 0.5, 0.5, 0.05);
-        player.getWorld().spawnParticle(Particle.SOUL_FIRE_FLAME, 
-            target.getLocation().add(0, 1, 0), 20, 0.5, 0.5, 0.5, 0.05);
-        
         return true;
-    }
-    
-    private void sendBindingHelp(Player player) {
-        player.sendMessage("");
-        player.sendMessage(ChatColor.DARK_PURPLE + "===== " + ChatColor.LIGHT_PURPLE + "SOUL BINDING RITUAL" + ChatColor.DARK_PURPLE + " =====");
-        player.sendMessage("");
-        player.sendMessage(ChatColor.GOLD + "/soulbind <player> " + ChatColor.GRAY + "- Bind your soul to another");
-        player.sendMessage(ChatColor.GRAY + "Allows sharing of Abyssal Tomes for 5 minutes");
-        player.sendMessage(ChatColor.DARK_RED + "WARNING: " + ChatColor.GRAY + "If you fall in combat during the binding...");
-        player.sendMessage(ChatColor.DARK_RED + "         " + ChatColor.GRAY + "Your Tome may fall into enemy hands!");
-        player.sendMessage("");
-        player.sendMessage(ChatColor.DARK_PURPLE + "=========================================");
     }
     
     @Override
