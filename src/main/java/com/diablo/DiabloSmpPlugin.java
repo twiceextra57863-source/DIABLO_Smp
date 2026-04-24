9dd77f81d5a95e5941412efe7c966214d76afdff
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-24 11:43:35 +0530

    Update DiabloSmpPlugin.java

--- src/main/java/com/diablo/DiabloSmpPlugin.java
+++ src/main/java/com/diablo/DiabloSmpPlugin.java
@@ -1,96 +1,44 @@
 package com.diablosmp;
 
-import com.diablosmp.abilities.AbilityManager;
-import com.diablosmp.commands.AbilityCommand;
+import com.diablosmp.ability.AbilityManager;
+import com.diablosmp.commands.DiabloCommand;
 import com.diablosmp.commands.TrustCommand;
-import com.diablosmp.listeners.AbilityListener;
-import com.diablosmp.listeners.BookListener;
-import com.diablosmp.listeners.TrustListener;
-import com.diablosmp.managers.CooldownManager;
-import com.diablosmp.managers.TrustManager;
-import com.diablosmp.managers.ParticleManager;
-import com.diablosmp.utils.ConfigManager;
+import com.diablosmp.listener.AbilityListener;
+import com.diablosmp.listener.BookListener;
+import com.diablosmp.listener.TrustListener;
+import com.diablosmp.utils.ConfigUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DiabloSmpPlugin extends JavaPlugin {
-    
     private static DiabloSmpPlugin instance;
     private AbilityManager abilityManager;
-    private CooldownManager cooldownManager;
-    private TrustManager trustManager;
-    private ParticleManager particleManager;
-    private ConfigManager configManager;
-    
+    private ConfigUtils configUtils;
+
     @Override
     public void onEnable() {
         instance = this;
-        
-        // Initialize managers
-        this.configManager = new ConfigManager(this);
-        this.cooldownManager = new CooldownManager();
-        this.trustManager = new TrustManager(this);
-        this.particleManager = new ParticleManager(this);
-        this.abilityManager = new AbilityManager(this);
-        
+        saveDefaultConfig();
+        configUtils = new ConfigUtils(this);
+        abilityManager = new AbilityManager(this);
+
+        // Register commands
+        getCommand("diablo").setExecutor(new DiabloCommand(this));
+        getCommand("trust").setExecutor(new TrustCommand(this));
+
         // Register listeners
         getServer().getPluginManager().registerEvents(new AbilityListener(this), this);
         getServer().getPluginManager().registerEvents(new BookListener(this), this);
         getServer().getPluginManager().registerEvents(new TrustListener(this), this);
-        
-        // Register commands with SIMPLE names
-        if (getCommand("ability") != null) {
-            getCommand("ability").setExecutor(new AbilityCommand(this));
-            getCommand("ability").setTabCompleter(new AbilityCommand(this));
-        }
-        
-        if (getCommand("trust") != null) {
-            getCommand("trust").setExecutor(new TrustCommand(this));
-            getCommand("trust").setTabCompleter(new TrustCommand(this));
-        }
-        
-        if (getCommand("diablo") != null) {
-            getCommand("diablo").setExecutor(new AbilityCommand(this));
-            getCommand("diablo").setTabCompleter(new AbilityCommand(this));
-        }
-        
-        getLogger().info("§5=================================");
-        getLogger().info("§d   Diablo SMP Plugin Enabled!");
-        getLogger().info("§5=================================");
-        getLogger().info("§7Commands: /ability, /trust, /diablo");
-        getLogger().info("§5=================================");
+
+        getLogger().info("DiabloSmpPlugin enabled!");
     }
-    
+
     @Override
     public void onDisable() {
-        if (abilityManager != null) {
-            abilityManager.saveAllData();
-        }
-        getLogger().info("§5=================================");
-        getLogger().info("§c   Diablo SMP Plugin Disabled!");
-        getLogger().info("§5=================================");
+        getLogger().info("DiabloSmpPlugin disabled.");
     }
-    
-    public static DiabloSmpPlugin getInstance() {
-        return instance;
-    }
-    
-    public AbilityManager getAbilityManager() {
-        return abilityManager;
-    }
-    
-    public CooldownManager getCooldownManager() {
-        return cooldownManager;
-    }
-    
-    public TrustManager getTrustManager() {
-        return trustManager;
-    }
-    
-    public ParticleManager getParticleManager() {
-        return particleManager;
-    }
-    
-    public ConfigManager getConfigManager() {
-        return configManager;
-    }
+
+    public static DiabloSmpPlugin getInstance() { return instance; }
+    public AbilityManager getAbilityManager() { return abilityManager; }
+    public ConfigUtils getConfigUtils() { return configUtils; }
 }
